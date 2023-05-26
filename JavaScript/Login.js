33650d820fb5e932f06d42a77130fa2e5ce00900
Mohsen Farrokhi <mfarrokhi1996@gmail.com>
2023-05-26 12:27:36 +0330

    Add & fix html and regex codes to JS

--- JavaScript/Login.js
+++ JavaScript/Login.js
@@ -1,19 +1,28 @@
 let inputUserName = document.getElementById('username');
 console.log(inputUserName);
+let noticeUserName = document.getElementById('username-notice')
+
 let inputEmail = document.getElementById('email')
 console.log(inputEmail);
+let noticeEmail = document.getElementById('email-notice')
+
 let inputPassword = document.getElementById('password')
 console.log(inputPassword);
+let noticePassword = document.getElementById('password-notice')
+
 
 inputUserName.addEventListener('change', checkUserName);
 function checkUserName() {
     let regexUserName = /^[a-zA-Z]+([\s-_.][a-zA-Z]+)*$/
+
     if (regexUserName.test(inputUserName.value)) {
         console.log("True");
-
+        inputUserName.style.border = "none";
+        noticeUserName.textContent = "";
     } else if (regexUserName) {
         console.log("False")
-        alert("Please input your Username correctly")
+        inputUserName.style.border = "2px solid red";
+        noticeUserName.textContent = "Invalid username. Please enter a valid Username.";
     }
 }
 
@@ -22,10 +31,12 @@ function checkEmail() {
     let regexEmail = /^\w+@\w*\.\w+$/
     if (regexEmail.test(inputEmail.value)) {
         console.log("True");
-
+       inputEmail.style.border = "none";
+        noticeEmail.textContent = "";
     } else if (regexEmail) {
         console.log("False")
-        alert("Please input your Email correctly")
+        inputEmail.style.border = "2px solid red";
+        noticeEmail.textContent = "Invalid username. Please enter a valid Email.";
     }
 }
 
@@ -34,9 +45,11 @@ function checkPassword() {
     let regexPassword = /^\w{8,}$/
     if (regexPassword.test(inputPassword.value)) {
         console.log("True");
-
+        inputPassword.style.border = "none";
+        noticePassword.textContent = "";
     } else if (regexPassword) {
         console.log("False")
-        alert("Please input your Password correctly")
+        inputPassword.style.border = "2px solid red";
+        noticePassword.textContent = "Invalid username. Please enter a valid Password.";
     }
 }
