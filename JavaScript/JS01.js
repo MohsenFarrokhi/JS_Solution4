fa526bec75f9ca5edfcee34366d1793e368e7837
Mohsen Farrokhi <mfarrokhi1996@gmail.com>
2023-05-27 08:29:17 +0330

    Add: const todo list element.

---
+++ JavaScript/JS01.js
@@ -0,0 +1,7 @@
+const todoInput = document.getElementById("todo-input");
+const addBtn = document.getElementById("add-btn");
+const todoList = document.getElementById("todo-list");
+const completedList = document.getElementById("completed-list");
+
+addBtn.addEventListener("click", addTodo);
+
